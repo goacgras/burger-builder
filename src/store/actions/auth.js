ca8ccd86191eb9d28dc6c1f6a51ed26120c07182
unknown <fachrezaghifary@gmail.com>
2020-07-21 15:30:54 +0700

    store token, add spinner, add error message

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -39,11 +40,11 @@ export const auth = (username, password, isSignup) => {
         axios.post(url, authData)
             .then(res => {
                 console.log(res);
-                dispatch(authSuccess(res.data));
+                dispatch(authSuccess(res.data.idToken, res.data.localId));
             })
             .catch(err => {
-                console.log(err);
-                dispatch(authFail(err));
+                console.log(err.response);
+                dispatch(authFail(err.response.data.error));
             });
     }
 };
