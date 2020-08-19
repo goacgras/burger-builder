8af68b846600f69a5f0f49e2aadda97d28e4305c
unknown <fachrezaghifary@gmail.com>
2020-08-19 10:31:21 +0700

    create index.js in sagas

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 
 import './index.css';
@@ -12,6 +13,7 @@ import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { watchAuth } from './store/sagas/index';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -30,7 +32,10 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
